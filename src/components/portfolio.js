23f58573b0f03893af2e544f5ff05ded8ab68f4e
adamrd231 <adam.rdconcepts@gmail.com>
2020-11-17 20:03:47 -0800

    update urls

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -17,7 +17,7 @@ function Portfolio(props) {
                             }
                         }}>
                             <img 
-                                src={`http://127.0.0.1:8000/${entry.image_url}`} 
+                                src={`https://website-portfolio-rdconcepts.herokuapp.com/${entry.image_url}`} 
                                 alt="Project Images"  >
                             </img>
                         </Link>
