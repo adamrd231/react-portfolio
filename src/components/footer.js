569778c26bc48804e58b33dd1385ca37dc608757
adamrd231 <adam.rdconcepts@gmail.com>
2020-12-15 19:48:36 -0800

    Update footer and blog layouts

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,10 +3,37 @@ import React from 'react';
 function Footer() {
     return (
         <div id="footer" className="footer">
+            <div className="footer-background">
+
+                
+
+                <div className="footer-text-container">
+                    <div className="footer-title-container">
+                        <p className="footer-title">rdConcepts</p>
+                        <p className="footer-text">Copyright RD Concepts, LLC All Rights Reserved.</p>
+                    </div>
+                    <div className="footer-text-container-two">
+                        <a href="mailto:someone@example.com">
+                            <p className="footer-text footer-link">contact@rdConcepts.design</p>
+                        </a>
+                        
+                        <p className="footer-text">231.445.1463</p>
+
+                        <a href="https://apps.apple.com/us/developer/rd-concepts/id1293498470">
+                            <p className="footer-text footer-link">App Store</p>
+                        </a>
+
+                        <a href="https://play.google.com/store/apps/developer?id=adamrd231">
+                            <p className="footer-text footer-link">Google Play Store</p>
+                        </a>
+                    </div>
+                    
+                </div>
+
+            </div>
 
             <div className="footer-icon-container">
-                <h2>Links</h2>
-                <div className="links-container">
+
                     <a href="https://www.instagram.com/adamrd231/">
                         <img className="footer-icons" src="https://portfolio-website-adamrd231.s3-us-west-1.amazonaws.com/insta-clean.png" alt="Instagram"/>    
                     </a>
@@ -16,31 +43,9 @@ function Footer() {
                     <a href="https://github.com/adamrd231">
                         <img className="footer-icons" src="https://portfolio-website-adamrd231.s3-us-west-1.amazonaws.com/github-clean.png" alt="Instagram"/>
                     </a>
-                </div>   
+
             </div>
   
-
-            <div>
-                <h2>About Me</h2>
-                <p>I live in Portland, Oregon.</p>
-                <p>Born and raised in Northern Michigan. </p>
-                <p>BFA in Human Centered Designfrom NMU.</p>
-            </div>
-
-            
-            <div className="footer-technology">
-                <h2>Technology</h2>
-                <p><strong>Design</strong> | Adobe Illustrator, UX, Photoshop, AfterEffects </p>
-                <p><strong>Dev</strong> | Python, Javascript, Swift, HTML/CSS, SQL</p>
-                <p><strong>This Website</strong> | Backend - Django, Frontend - React.js</p>
-            </div>
-
-
-            <div>
-                <h2>Contact</h2>
-                <p></p>
-                <p>adam@rdconcepts.design</p>
-            </div>
             
         </div>
     )
--- src/components/menu.js
+++ src/components/menu.js
@@ -8,10 +8,10 @@ function Menu() {
        
         <div className="menu">
          
-            <Link to="/" className="link clickable">Home</Link>
-            <HashLink to="#blogs" className="link clickable">Blogs</HashLink>
-            <HashLink to="#portfolio" className="link clickable">Portfolio</HashLink>
-            <HashLink to="#footer" className="link clickable">Contact</HashLink>
+            <Link to="/" className="menu-item link clickable">Home</Link>
+            <HashLink to="#blogs" className="menu-item link clickable">Blogs</HashLink>
+            <HashLink to="#portfolio" className="menu-item link clickable">Portfolio</HashLink>
+            <HashLink to="#footer" className="menu-item link clickable">Contact</HashLink>
 
         </div>
         
