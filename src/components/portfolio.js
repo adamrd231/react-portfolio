f65e75c30bb3c1c7079b8045c044cc798eaa28e9
adamrd231 <adam.rdconcepts@gmail.com>
2021-01-04 14:15:18 -0800

    Hover design to show description only when mouse is over projects.

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,44 +1,58 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import PortfolioHover from './portfolio-hover';
 
 function Portfolio(props) {
 
+    const [ hovering, SetHovering ] = useState(true);
+    const [ customClassName, SetCustomClassName ] = useState("hidden");
+    const hiddenComponent = "hidden"
+    const showComponent = "visible"
+
+    const handleMouseHover = () => {
+        SetHovering(!hovering)
+        console.log(hovering)
+    }
+
+    useEffect(() => {
+        if (hovering == false) {
+            SetCustomClassName('visible')
+        } else {
+            SetCustomClassName('hidden')
+        }
+    },[hovering, SetHovering])
 
     return (
         <div id="portfolio" className="portfolio-container"> 
 
-            <div className="portfolio-box">
+            <div className="portfolio-box" onMouseEnter={handleMouseHover} onMouseLeave={handleMouseHover}>
+                
                 { props.entries && props.entries.slice(0, 3).map( entry => {    
-                        return (
-                            <div className="portfolio-entry clickable" id={entry.slug} key={entry.id}>
-                                <Link
-                                    className="home-page-portfolio"
-                                    to={{
+                    return (
+                        <div className="portfolio-entry clickable" id={entry.slug} key={entry.id}>
+                            <Link
+                                className="home-page-portfolio"
+                                to={{
                                     pathname: "/portfolio/" + entry.slug,
-                                    state: {
-                                        entry: entry
-                                    }
-                                }}>
-                                    <img 
-                                        src={`https://portfolio-website-adamrd231.s3-us-west-1.amazonaws.com/media/${entry.image_url}`} 
-                                        alt="Project Images"  >
-                                    </img>
-                                    <h2 className="portfolio-t">{entry.title}</h2>
-                                    <div className="portfolio-desciption">
-                                        <p>{entry.description.slice(0, 50)}</p>
-                                        <p>"Click to read more..."</p>
-                                    </div>
-                                    
-                                    
-                            
-                                </Link>
-                            
-                            </div>
-                        )
+                                    state: { entry: entry }}}>
+
+                                <img 
+                                    src={`https://portfolio-website-adamrd231.s3-us-west-1.amazonaws.com/media/${entry.image_url}`} 
+                                    alt="Project Images"  >
+                                </img>
+
+                                <h2 className="portfolio-t">{entry.title}</h2>
+                                <div className={customClassName}>
+                                    <span dangerouslySetInnerHTML = {{ __html: entry.description.slice(0, 75) + "..."}}></span>
+                                    <p>Click to read more.</p>
+                                </div>
+                            </Link>      
+                        </div>
+                    )
                 })}
             </div>
             
-            <div className="porfolio-slug-section">
+            <div className={"porfolio-slug-section"}>
                 <p>For more app development, UX and UI design, Illustrations or to check out my work, see where I have been traveling, what I have been designing. click the link below.</p>
                 <Link>
                     <h2>Explore full portfolio.</h2>
