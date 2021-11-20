efd14f45907df900567adeb3b9f124b453519991
adamrd231 <adam.rdconcepts@gmail.com>
2021-11-19 21:17:47 -0700

    Update links to open in new tab

--- src/components/menu-icons.js
+++ src/components/menu-icons.js
@@ -4,13 +4,13 @@ function MenuIcons() {
 
     return (
         <div className="icons">
-            <a href="https://www.instagram.com/adamrd231/">
+            <a href="https://www.instagram.com/adamrd231/" target="_blank" rel="noopener noreferrer">
                 <img src="https://portfolio-website-adamrd231.s3-us-west-1.amazonaws.com/insta-clean.png" alt="Instagram"/>    
             </a>
-            <a href="https://www.linkedin.com/in/rdconcepts/">
+            <a href="https://www.linkedin.com/in/rdconcepts/" target="_blank" rel="noopener noreferrer">
                 <img src="https://portfolio-website-adamrd231.s3-us-west-1.amazonaws.com/linkedin-clean.png" alt="Instagram"/>
             </a>
-            <a href="https://github.com/adamrd231">
+            <a href="https://github.com/adamrd231" target="_blank" rel="noopener noreferrer">
                 <img src="https://portfolio-website-adamrd231.s3-us-west-1.amazonaws.com/github-clean.png" alt="Instagram"/>
             </a>
         </div>
